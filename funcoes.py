14ae64b72f9148aecbf3afbdb4670cfa165242f6
Eduardo Vieira <eduardovieira2@gmail.com>
2018-09-28 20:57:14 -0300

    Inclusão de comentários para explicar o que faz o que

--- funcoes.py
+++ funcoes.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 #Sai do programa
-def sair():	
+def sair():
 	exit()
 
 #Limpa a tela
@@ -21,7 +21,7 @@ def bemvindo():
 	print("2 - Listar os contatos")
 	print("3 - Sair")
 	#Recupera a opcao selecionada
-	opcao = int(input())
+	opcao = int(input())	
 	#Estrutura de controle
 	if opcao == 1:
 		adicionar()
@@ -40,45 +40,65 @@ def adicionar():
 	print("------------------------------------------")
 	print("Adicionar um Contato")
 	print("------------------------------------------")	
-	#abre o arquivo agenda
+	#abre o arquivo agenda para escrita
 	agenda = open("agendatelefonica.csv",'a')
 	#recupera o nome digitado na tela
 	nome = input("Nome do Contato:")
 	#recupera o telefone digitado na tela
 	telefone = input("Digite o telefone:")	
+	#limpa a tela
 	clear()
+	#escreve o header de sucesso
 	print("------------------------------------------")	
 	print("CONTATO SALVO COM SUCESSO!")
 	print("------------------------------------------")	
+	#printo o nome e o telefone, com tabulacao
+	#esse e um ponto que pode ser melhorado, porque a tabulacao nao fica alinhada
 	print("Nome\t\t\tTelefone")
 	print(nome, "\t\t\t", telefone)
+	#escrevo no arquivo o nome do contato
 	agenda.write(nome)
+	#acrescento uma vírgula, para separar os dados
 	agenda.write(",")
+	#adiciono o telefone
 	agenda.write(telefone)
+	#quero uma linha
 	agenda.write("\n")
+	#fecho o arquivo
 	agenda.close()
-	opcoes()
-	bemvindo()
+	#exibo a mensagem para pressionar enter para ir para opcoes
+	opcoes()	
 	
 def listar():
+	#limpa a tela
 	clear()
+	#escreve o header
 	print("------------------------------------------")
 	print("Lista de Contatos")
 	print("------------------------------------------")	
-	
+	#abre o arquivo agenda somente leitura
 	agenda = open("agendatelefonica.csv")
-	for linha in agenda:		
+	#para cada linha do arquivo
+	for linha in agenda:
+		#eu separo o nome do telefone pelo caractere ','
 		arr = linha.replace('\n','').split(",")
+		#pego o nome
 		nome = arr[0]
+		#pego o telefone
 		telefone = arr[1]
+		#e printo na tela o nome e o telefone, com tabulacao
 		print (nome, "\t\t\t", telefone)
+	#fecha o arquivo
 	agenda.close()
+	#exibo a mensagem para pressionar enter para ir para opcoes
 	opcoes()
 
 def opcoes():
 	input("Pressione Enter para voltar as opcoes...")
+	#se apertar, ele exibe o menu
 	bemvindo()
 
 def falha():
+	#exibe a mensagem de opcao incorreta
 	print("Opcao Incorreta")
 	opcoes()
